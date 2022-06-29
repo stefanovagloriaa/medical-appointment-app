5ab93a86a7d77273986f5210137cf8154daa10cd
Gloria Stefanova <stefanovagloria97@gmail.com>
2022-06-29 13:14:53 +0300

    MainNavigation edited

--- components/MainNavigation.js
+++ components/MainNavigation.js
@@ -1,7 +1,33 @@
+import { Fragment } from "react";
+import Link from "next/link";
 import classes from "./MainNavigation.module.css";
 
 const MainNavigation = () => {
-  return <h1>MainNavigation</h1>;
+  return (
+    <Fragment>
+      <div className={classes.topnav}>
+        <Link href="/">
+          <li>Начало</li>
+        </Link>
+        <Link href="/services">
+          <li>Услуги</li>
+        </Link>
+        <Link href="/gallery">
+          <li>Галерия</li>
+        </Link>
+        <Link href="/about">
+          <li>Вашият лекар</li>
+        </Link>
+        <Link href="/contacts">
+          <li>Контакти</li>
+        </Link>
+        <Link href="/login">
+          <li>Вход</li>
+        </Link>
+        <button className={classes.buttonStyle}>Запази час</button>
+      </div>
+    </Fragment>
+  );
 };
 
 export default MainNavigation;
