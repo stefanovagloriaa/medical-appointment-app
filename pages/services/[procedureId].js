5eca3116673ed650bbaa76c403df5415fecb0dca
Gloria Stefanova <stefanovagloria97@gmail.com>
2022-06-29 11:59:57 +0300

    Enter e message here

---
+++ pages/services/[procedureId].js
@@ -0,0 +1,5 @@
+const ProcedureDetail = () => {
+  return <h1>ProcedureDetail Page</h1>;
+};
+
+export default ProcedureDetail;
---
+++ pages/services/index.js
@@ -0,0 +1,5 @@
+const ServicesPage = () => {
+  return <h1>Service Page</h1>;
+};
+
+export default ServicesPage;
