88828f47006791f5e94d05486f06604ca97058ec
Gloria Stefanova <stefanovagloria97@gmail.com>
2022-06-29 13:02:45 +0300

    AddressNavigation edited

--- components/AddressNavigation.js
+++ components/AddressNavigation.js
@@ -1,7 +1,15 @@
 import classes from "./AddressNavigation.module.css";
 
 const AddressNavigation = () => {
-  return <h1>AddressNavigation</h1>;
+  return (
+    <div className={classes.element}>
+      <span>
+        Александър Велики 41, <b>гр.Бургас</b>
+      </span>
+      <span>md.nikolov91@gmail.com</span>
+      <span>+359 78 1000 99</span>
+    </div>
+  );
 };
 
 export default AddressNavigation;
