8a3df2124835c107f2017b87c35e4b6ba7d04af1
Gloria Stefanova <stefanovagloria@abv.bg>
2022-07-03 11:08:33 +0300

    verification message added

--- components/Verification.js
+++ components/Verification.js
@@ -1,7 +1,15 @@
+import { Fragment } from "react";
 import classes from "./Verification.module.css";
 
 const Verification = () => {
-  return <h1>Verification Component</h1>;
+  return (
+    <div className={classes.message}>
+      Hello! You registered your account! Before being able to use your account
+      you need to verify that this is your email address.
+      <br></br>
+      Kind Regards!
+    </div>
+  );
 };
 
 export default Verification;
