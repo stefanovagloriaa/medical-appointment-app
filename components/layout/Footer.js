f7dd320e7992b1913303b0fc0c4a27635c7af843
Gloria Stefanova <stefanovagloria@abv.bg>
2022-07-06 07:37:29 +0300

    component edited

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -1,9 +1,25 @@
+import Link from "next/link";
 import classes from "./Footer.module.css";
 
 const Footer = () => {
   return (
     <div className={classes.footerContainer}>
       <h1>Footer</h1>
+      <Link href="/services">
+        <li>Услуги</li>
+      </Link>
+      <Link href="/gallery">
+        <li>Галерия</li>
+      </Link>
+      <Link href="/contacts">
+        <li>Контакти</li>
+      </Link>
+      <Link href="/login">
+        <li>Вход</li>
+      </Link>
+      <Link href="/signUp">
+        <li>Регистрирай се</li>
+      </Link>
     </div>
   );
 };
