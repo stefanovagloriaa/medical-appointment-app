a123f3a9248403a6b6617631aeac6f4c4af3d5d6
Gloria Stefanova <stefanovagloria@abv.bg>
2022-07-11 08:17:35 +0300

    emailjs sendEmail function added

--- components/AppointmentModal.js
+++ components/AppointmentModal.js
@@ -1,13 +1,35 @@
 import classes from "./AppointmentModal.module.css";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import emailjs from "@emailjs/browser";
 
 const AppointmentModal = () => {
   const [showModal, setShowModal] = useState(false);
+  const form = useRef();
 
   const closeModal = () => {
     setShowModal(false);
   };
+
+  const submitHandler = (ev) => {
+    ev.preventDefault();
+
+    emailjs
+      .sendForm(
+        "contact_service",
+        "contact_form",
+        form.current,
+        "FbAECAXTkZfH0x-Y1"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <div>
       <div>
@@ -19,13 +41,19 @@ const AppointmentModal = () => {
         </button>
       </div>
       {showModal && (
-        <form className={classes.appointment}>
+        <form
+          ref={form}
+          onSubmit={submitHandler}
+          className={classes.appointment}
+        >
           <h1>Save your hour</h1>
-          <label id="subject">Тема</label>
-          <input id="subject" />
-          <label htmlFor="message">Съобщение</label>
-          <textarea id="message"></textarea>
-          <button>Send email</button>
+          <label>Име</label>
+          <input type="text" name="firstName" />
+          <label>Имейл</label>
+          <input type="email" name="userEmail" />
+          <label>Съобщение</label>
+          <textarea name="message"></textarea>
+          <input type="submit" value="Send" />
           <button onClick={() => setShowModal(false)}>No</button>
         </form>
       )}
