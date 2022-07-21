4fe0491bb773e87314b18fd20ca7e7689f575065
Gloria Stefanova <stefanovagloria@abv.bg>
2022-07-21 08:44:18 +0300

    next/head added

--- pages/signUp.js
+++ pages/signUp.js
@@ -1,7 +1,16 @@
+import Head from "next/head";
+import { Fragment } from "react";
 import Register from "../components/Register";
 
 const SignUpPage = () => {
-  return <Register />;
+  return (
+    <Fragment>
+      <Head>
+        <title>Register</title>
+      </Head>
+      <Register />
+    </Fragment>
+  );
 };
 
 export default SignUpPage;
