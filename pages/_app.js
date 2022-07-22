a8cf836a04a4b6397577f2c5ca8cd9f546b148fc
Gloria Stefanova <stefanovagloria@abv.bg>
2022-07-22 16:10:07 +0300

    bootstrap imported in _app.js file

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,22 @@
 import { Provider } from "react-redux";
 import store from "../store/index";
 import Layout from "../components/layout/Layout";
+import Head from "next/head";
+import "bootstrap/dist/css/bootstrap.css";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Provider store={store}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </Provider>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Provider store={store}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Provider>
+    </>
   );
 }
 
